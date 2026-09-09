aa5145f88130ceaf12b07b98e78311a5f93740c3
nana.osei <oseinanayaw28@gmail.com>
2026-09-09 09:19:02 +0200

    Player movement implemented

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput();
+    }
+    private void FixedUpdate()
+        {
+        Move();
+        }
+    private void PlayerInput ()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+    
+}
