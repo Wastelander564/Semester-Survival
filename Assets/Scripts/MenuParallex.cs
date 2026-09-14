4a7c4ecd106b26acf75819bfc5dd903211c44724
nana.osei <oseinanayaw28@gmail.com>
2026-09-14 10:09:34 +0200

    Restore menu work

--- Assets/Scripts/MenuParallex.cs
+++ Assets/Scripts/MenuParallex.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class MenuParallex : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float offsetMultiplier = 1f;
+    public float smoothTime = .3f;
+
+    private Vector2 startPosition;
+    private Vector3 velocity;
+    private void Start()
     {
-        
+        startPosition = transform.position; 
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
     }
 }
